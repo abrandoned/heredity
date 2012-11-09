d0b9f3f17c770ee162c5e36e9e871e8637977b98
Brandon Dewitt <brandonsdewitt@gmail.com>
2012-11-08 17:52:08 -0700

    start with modules in their own files

--- lib/heredity/version.rb
+++ lib/heredity/version.rb
@@ -1,3 +1,3 @@
 module Heredity
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
--- spec/inheritable_class_instance_variables_spec.rb
+++ spec/inheritable_class_instance_variables_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'heredity/inheritable_class_instance_variables'
 
 describe Heredity::InheritableClassInstanceVariables do
 
