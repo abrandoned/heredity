e8ac1fb438370f972fecf71666d0dddadc8100cc
Adam Hutchison <liveh2o@gmail.com>
2013-09-30 17:39:09 -0600

    Don't eagerly initialize inherited hooks.

--- lib/heredity/core_ext/class.rb
+++ lib/heredity/core_ext/class.rb
@@ -1,5 +1,9 @@
 class Class
   def inherited(klass)
+    # Check the class instance variable so we don't eagerly initialize empty
+    # arrays for every class (i.e. object).
+    return if @inherited_hooks.nil?
+
     inherited_hooks.each do |block|
       klass.class_eval(&block)
     end
